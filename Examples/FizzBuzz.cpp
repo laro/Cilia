e05beda63bb90ea365aaba21442f1cbcf0a4cf35
Lars Rosenboom <mail@larsrosenboom.de>
2025-10-27 23:33:40 +0100

    Update FizzBuzz.cpp

--- Examples/FizzBuzz.cpp
+++ Examples/FizzBuzz.cpp
@@ -1,12 +1,12 @@
 void fizzBuzz(int count) {
     for (int i = 1; i <= count; ++i) {
         if (i % 15 == 0)
-            std::cout << "FizzBuzz\n";
+            cout << "FizzBuzz\n";
         else if (i % 3 == 0)
-            std::cout << "Fizz\n";
+            cout << "Fizz\n";
         else if (i % 5 == 0)
-            std::cout << "Buzz\n";
+            cout << "Buzz\n";
         else
-            std::cout << i << '\n';
+            cout << i << '\n';
     }
 }
