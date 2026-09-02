8f8e8f20d7da9bfd6712c019ceb9945abf78a232
Lars Rosenboom <mail@larsrosenboom.de>
2026-09-02 18:25:58 +0200

    ...

--- 4 Examples/SieveOfEratosthenes.cpp
+++ 4 Examples/SieveOfEratosthenes.cpp
@@ -9,11 +9,10 @@ auto sieve_of_eratosthenes(int n) -> vector<int> {
 
     int limit = static_cast<int>(sqrt(n));
     for (int i = 2; i <= limit; ++i) {
-        if (!is_prime[i]) {
-            continue;
-        }
-        for (int j = i*i; j <= n; j += i) {
-            is_prime[j] = false;
+        if (is_prime[i]) {
+            for (int j = i*i; j <= n; j += i) {
+                is_prime[j] = false;
+            }
         }
     }
 
