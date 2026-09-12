e2bd8cec716c4c668d3a1d3ade79483ec2f390dc
Lars Rosenboom <mail@larsrosenboom.de>
2026-09-12 14:19:38 +0200

    isqrt

--- 4 Examples/SieveOfEratosthenes.cpp
+++ 4 Examples/SieveOfEratosthenes.cpp
@@ -2,12 +2,12 @@ auto sieve_of_eratosthenes(int n) -> vector<int> {
     if (n < 2) {
         return {};
     }
-    
+
     vector<bool> is_prime(n + 1, true);
     is_prime[0] = false;
     is_prime[1] = false;
 
-    int limit = static_cast<int>(sqrt(n));
+    int limit = isqrt(n);
     for (int i = 2; i <= limit; ++i) {
         if (is_prime[i]) {
             for (int j = i*i; j <= n; j += i) {
@@ -23,6 +23,6 @@ auto sieve_of_eratosthenes(int n) -> vector<int> {
             primes.push_back(i);
         }
     }
-    
+
     return primes;
 }
